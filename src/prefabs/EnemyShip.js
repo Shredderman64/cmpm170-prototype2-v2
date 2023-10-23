25231e592db23bc84b90b99704ebc7aa7d61e7aa
Atri Mehta <apmehta@ucsc.edu>
2023-10-23 14:25:24 -0700

    pushin image

--- src/prefabs/EnemyShip.js
+++ src/prefabs/EnemyShip.js
@@ -1,5 +1,5 @@
 // EnemySpaceship prefab
-class EnemySpaceship extends Spaceship {
+class EnemySpaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x, y, texture, frame, pointValue);
         scene.add.existing(this); // add object to existing scene
