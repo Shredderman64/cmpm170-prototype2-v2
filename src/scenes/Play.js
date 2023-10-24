60b4dd8da1c9a89551813bc0310eae01c829364f
Atri Mehta <apmehta@ucsc.edu>
2023-10-24 00:13:26 -0700

    trying to add mouse

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -45,7 +45,7 @@ class Play extends Phaser.Scene {
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
 
-        this.enemyShip = new EnemySpaceship(this, game.config.width, borderUISize*7 + borderPadding*6, 'enemySpaceship', 0, 50).setOrigin(0,0);
+        this.enemyShip = new EnemySpaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'enemySpaceship', 0, 50).setOrigin(0,0);
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
