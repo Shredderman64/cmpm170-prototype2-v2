54038d0b3af0623f9df1e7e40a447fb687bef3f3
Atri Mehta <apmehta@ucsc.edu>
2023-10-23 22:13:49 -0700

    trying to add mouse

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -31,6 +31,8 @@ class Play extends Phaser.Scene {
             this.p1Rocket.y = pointer.y;
         }, this);
 
+        
+
         // add spaceships (x3)
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
