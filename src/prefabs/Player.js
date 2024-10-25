75ee0cec2cc07d72fd3498ae59d542809058d1a3
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-10-24 19:46:05 -0700

    Added left-right keys

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -1,9 +1,27 @@
 class Player extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame) {
+    constructor(scene, x, y, texture, frame, controls) {
         super(scene, x, y, texture, frame);
+
+        this.left = controls.left;
+        this.right = controls.right;
+        this.up = controls.up;
+        this.down = controls.down;
+
+        this.playerSpeed = 10;
 
         scene.add.existing(this);
 
         return this;
+    }
+
+    update() {
+        if (this.left.isDown) {
+            if (this.x > (this.displayWidth / 2))
+                this.x -= this.playerSpeed;
+        }
+        if (this.right.isDown) {
+            if (this.x < (game.config.width - this.displayWidth / 2))
+                this.x += this.playerSpeed;
+        }
     }
 }
--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -13,9 +13,22 @@ class Level extends Phaser.Scene {
 
     create() {
         let my = this.my;
-        let cfg = game.config;
 
-        my.sprite.player = new Player(this, cfg.width / 5, cfg.height / 2, "player", null);
+        this.controls = {};
+
+        this.controls.left = this.input.keyboard.addKey("A");
+        this.controls.right = this.input.keyboard.addKey("D");
+        this.controls.up = this.input.keyboard.addKey("W");
+        this.controls.down = this.input.keyboard.addKey("S");
+
+        my.sprite.player = new Player(this, game.config.width / 6, game.config.height / 2,
+            "player", null, this.controls);
         my.sprite.player.setScale(0.25);
+    }
+
+    update() {
+        let my = this.my;
+
+        my.sprite.player.update();
     }
 }
