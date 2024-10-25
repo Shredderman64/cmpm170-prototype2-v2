16d439587d20fadc9fcea7933da238a57de7da67
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-10-24 19:31:03 -0700

    Added player to scene

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -1,5 +1,21 @@
 class Level extends Phaser.Scene {
     constructor() {
         super("levelScreen");
+
+        this.my = {sprite: {}};
+    }
+
+    preload() {
+        this.load.setPath("./assets/");
+
+        this.load.image("player", "player.png");
+    }
+
+    create() {
+        let my = this.my;
+        let cfg = game.config;
+
+        my.sprite.player = new Player(this, cfg.width / 5, cfg.height / 2, "player", null);
+        my.sprite.player.setScale(0.25);
     }
 }
